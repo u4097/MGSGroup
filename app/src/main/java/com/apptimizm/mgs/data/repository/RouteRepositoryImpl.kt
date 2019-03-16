83bbfae5c384bf532a403b1f7d3bc9591a8e4106
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-03-16 13:07:57 +0300

    get routes from cache

--- app/src/main/java/com/apptimizm/mgs/data/repository/RouteRepositoryImpl.kt
+++ app/src/main/java/com/apptimizm/mgs/data/repository/RouteRepositoryImpl.kt
@@ -3,6 +3,8 @@ package com.apptimizm.mgs.data.repository
 import com.apptimizm.mgs.data.datasource.RouteCacheDataSource
 import com.apptimizm.mgs.data.datasource.RouteRemoteDataSource
 import com.apptimizm.mgs.data.repository.resouces.Resource
+import com.apptimizm.mgs.data.repository.resouces.ResourceState
+import com.apptimizm.mgs.datasource.model.route.RouteResponseEntity
 import com.apptimizm.mgs.datasource.model.route.mapToDomain
 import com.apptimizm.mgs.domain.model.route.RouteResponseModel
 import com.apptimizm.mgs.domain.repository.RouteRepository
@@ -15,17 +17,9 @@ class RouteRepositoryImpl constructor(
 ) : RouteRepository {
 
     override suspend fun get(page: String, pageSize: String): Resource<RouteResponseModel>? {
-/*        when (refresh) {
-            true -> {
-                val routes = remoteDataSource.get()
-                // Save to cache
-                cacheDataSource.set(routes = routes?.data!!)
-                return routes.mapToDomain()
-            }
-        }*/
-/*        var routeList: RouteResponseEntity?
+        var routeList: RouteResponseEntity?
         // Get from cache
-        var response = cacheDataSource.get()
+        val response = cacheDataSource.get()
         response.await().let {
             routeList = it
         }
@@ -34,7 +28,7 @@ class RouteRepositoryImpl constructor(
                 Timber.d("Get route from cache")
                 return Resource(ResourceState.SUCCESS, routeList?.mapToDomain())
             }
-        }*/
+        }
 
         val routes = remoteDataSource.get(page, pageSize)
         Timber.d("Get route from server:\n ${routes?.data?.results}")
