1486b67d4e8d35929ecab73fec379135cd707e1b
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-03-16 13:36:25 +0300

    import resources

--- app/src/main/java/com/apptimizm/mgs/data/repository/RouteRepositoryImpl.kt
+++ app/src/main/java/com/apptimizm/mgs/data/repository/RouteRepositoryImpl.kt
@@ -18,26 +18,27 @@ class RouteRepositoryImpl constructor(
 
     override suspend fun get(page: String, pageSize: String): Resource<RouteResponseModel>? {
         var routeList: RouteResponseEntity?
-        // Get from cache
+        // Get from cache first.
         val response = cacheDataSource.get()
         response.await().let {
             routeList = it
         }
         routeList?.let {
             if (!routeList?.results?.isEmpty()!!) {
-                Timber.d("Get route from cache")
+                Timber.d("Get routes from cache!")
                 return Resource(ResourceState.SUCCESS, routeList?.mapToDomain())
             }
         }
 
+        // Get from server.
         val routes = remoteDataSource.get(page, pageSize)
-        Timber.d("Get route from server:\n ${routes?.data?.results}")
+        Timber.d("Get routes from server:\n ${routes?.data?.results}")
         routes?.data?.results?.let {
             try {
                 // Save to cache
                 cacheDataSource.set(routes = routes.data)
             } catch (e: Exception) {
-                Timber.e("Can't save route into cache: ${e.stackTrace}")
+                Timber.e("Can't save routes into cache:\n ${e.stackTrace}")
             }
         }
         return routes?.mapToDomain()
